a007371cb999f65153998d104cddb0d498ded665
Stiveys <stevomwaura@gmail.com>
2024-11-12 10:18:38 +0300

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import logo from "./assets/logo.ico"
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <div className="w-full h-full absolute bg-gradient-to-r from-white to-cyan-100">
+      <header className="flex justify-between items-center text-black py-6 px-8
+md:px-32 bg-white drop-shadow-md
+      ">
+        <a href="#">
+          <img src={logo} alt="" className="w-52 hover:scale-105 transition-all" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        <ul className="hidden xl:flex items-center gap-12 font-semibold text-base">
+        <li classname="p-3 hover:bg-sky-300 hover:text-white rounded-md transition-all cursor-pointer">Home</li>
+        <li classname="p-3 hover:bg-sky-300 hover:text-white rounded-md transition-all cursor-pointer">Book Now</li>
+        <li classname="p-3 hover:bg-sky-300 hover:text-white rounded-md transition-all cursor-pointer">About</li>
+        <li classname="p-3 hover:bg-sky-300 hover:text-white rounded-md transition-all cursor-pointer">Contact</li>
+        </ul>
+
+        <div className="relative hidden md:flex items-center justify-center gap-3"></div>
+        <i className="bx bx-search absolute"></i>
+        <input type="text" placeholder="Search..." className="py-1 pl-5 rounded -x1 border-2 border-sky-200 focus:bg-slate-100 focus:outline-sky-200"/>
+      </header>
+    </div>
+
+    
   )
 }
 
